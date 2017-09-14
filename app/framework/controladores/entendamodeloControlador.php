d0dbe9946d32d71fb58cb257a12e9edde63bcbef
ManualDeveloper <lucas.eduardo.developer@gmail.com>
2017-09-14 08:05:19 -0300

    Add files via upload

---
+++ app/framework/controladores/entendamodeloControlador.php
@@ -0,0 +1,19 @@
+<?php
+
+    class entendamodeloControlador extends Controller{
+
+        public function indexAcao(){
+
+            $parametros['url']= $this->url;
+
+			$parametros['controlador']= $this->router->controlador;
+		
+			$parametros['acao']= $this->router->acao;
+
+            $this->load->visao('indexVisao.phtml', $parametros);
+
+        }
+
+    }
+
+?>
